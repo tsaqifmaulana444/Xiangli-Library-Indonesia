32a76305e84227c1662e793a9bda5f3927f48147
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2024-02-28 08:48:49 +0700

    wip: books borrow

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Book extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
