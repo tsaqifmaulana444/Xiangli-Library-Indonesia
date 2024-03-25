557d6ca28a3beaf1bf7ee64ab53a6b1b5573d042
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2024-03-25 10:11:24 +0700

    feat: plus trigger

--- database/migrations/2024_03_25_023405_minus_trigger.php
+++ database/migrations/2024_03_25_023405_minus_trigger.php
@@ -10,15 +10,30 @@ return new class extends Migration
     public function up()
     {
         DB::unprepared('
-        CREATE OR REPLACE FUNCTION reduce_stock() RETURNS TRIGGER AS $$
-        BEGIN
-            UPDATE books SET stock = stock - NEW.amount WHERE id = NEW.book_id;
-            RETURN NEW;
-        END;
-        $$ LANGUAGE plpgsql;
+            CREATE OR REPLACE FUNCTION reduce_stock() RETURNS TRIGGER AS $$
+            BEGIN
+                UPDATE books SET stock = stock - NEW.amount WHERE id = NEW.book_id;
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+            
+            CREATE TRIGGER tr_minus AFTER INSERT ON book_user FOR EACH ROW
+            WHEN (NEW.status = \'Waiting\')
+            EXECUTE FUNCTION reduce_stock();
+        ');
 
-        CREATE TRIGGER tr_minus AFTER INSERT ON book_user FOR EACH ROW
-        EXECUTE FUNCTION reduce_stock();
+        // Trigger for increasing stock when a book is returned
+        DB::unprepared('
+            CREATE OR REPLACE FUNCTION increase_stock() RETURNS TRIGGER AS $$
+            BEGIN
+                UPDATE books SET stock = stock + NEW.amount WHERE id = NEW.book_id;
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;
+            
+            CREATE TRIGGER tr_plus AFTER UPDATE ON book_user FOR EACH ROW
+            WHEN (NEW.status = \'Done\' AND OLD.status != \'Done\')
+            EXECUTE FUNCTION increase_stock();
         ');
     }
 
@@ -26,5 +41,8 @@ return new class extends Migration
     {
         DB::unprepared('DROP TRIGGER IF EXISTS tr_minus ON book_user;');
         DB::unprepared('DROP FUNCTION IF EXISTS reduce_stock();');
+
+        DB::unprepared('DROP TRIGGER IF EXISTS tr_plus ON book_user;');
+        DB::unprepared('DROP FUNCTION IF EXISTS increase_stock();');
     }
 };
