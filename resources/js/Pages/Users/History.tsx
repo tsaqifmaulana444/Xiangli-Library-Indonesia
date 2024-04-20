e7f7c98cfb70719d89e4a5fe88f5b450cf3969df
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2024-04-20 13:11:00 +0700

    rating

--- resources/js/Pages/Users/History.tsx
+++ resources/js/Pages/Users/History.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import { Inertia } from '@inertiajs/inertia'
 import { useState } from 'react'
 import AmendBookModal from './modals/AmendBookModal'
+import BookRatingModal from './modals/BookRatingModal'
 import { useLaravelReactI18n } from 'laravel-react-i18n'
 import toast, { Toaster } from 'react-hot-toast'
 
@@ -29,6 +30,9 @@ export default function History({ borrows, name, email }: PageProps<{ borrows: B
     toast.success('Success!')
   }
 
+  const [isRatingModalOpen, setIsRatingModalOpen] = useState(false)
+  const [selectedRating, setSelectedRating] = useState<Borrow | null>(null)
+
   const [isAmendModalOpen, setIsAmendModalOpen] = useState(false)
   const [selectedBorrow, setSelectedBorrow] = useState<Borrow | null>(null)
 
@@ -40,6 +44,16 @@ export default function History({ borrows, name, email }: PageProps<{ borrows: B
   const closeAmendModal = () => {
     setSelectedBorrow(null)
     setIsAmendModalOpen(false)
+  }
+  
+  const openRatingModal = (borrow: Borrow) => {
+    setSelectedRating(borrow)
+    setIsRatingModalOpen(true)
+  }
+
+  const closeRatingModal = () => {
+    setSelectedRating(null)
+    setIsRatingModalOpen(false)
   }
 
   const doneBorrow = async (id: string | undefined) => {
@@ -59,6 +73,7 @@ export default function History({ borrows, name, email }: PageProps<{ borrows: B
   return (
     <>
       {isAmendModalOpen && selectedBorrow && <AmendBookModal closeModal={closeAmendModal} borrow={selectedBorrow} />}
+      {isRatingModalOpen && selectedRating && <BookRatingModal closeModal={closeRatingModal} borrow={selectedRating} />}
       <Head>
         <title>{appName}</title>
       </Head>
@@ -101,6 +116,9 @@ export default function History({ borrows, name, email }: PageProps<{ borrows: B
                       <th scope="col" className="px-6 py-3 text-center">
                         {t('user_book17')}
                       </th>
+                      <th scope="col" className="px-6 py-3 text-center">
+                        Rate The Book
+                      </th>
                     </tr>
                   </thead>
                   <tbody>
@@ -115,7 +133,7 @@ export default function History({ borrows, name, email }: PageProps<{ borrows: B
                       borrows.map((borrow, index) => (
                         <tr key={borrow.id} className={index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
                           <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{index + 1}</td>
-                          <td className="px-6 py-4 text-center">{borrow.book.name}</td>
+                          <td className="px-6 py-4 text-center">{borrow.book_id}</td>
                           <td className="px-6 py-4 text-center">{borrow.amount}</td>
                           <td className="px-6 py-4 text-center">{borrow.borrow_in}</td>
                           <td className="px-6 py-4 text-center">{borrow.borrow_out}</td>
@@ -134,15 +152,26 @@ export default function History({ borrows, name, email }: PageProps<{ borrows: B
                               <button type="button" className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 mb-2" onClick={() => doneBorrow(borrow.id)}>{t('user_book19')}</button>
                             )}
                           </td>
-                          {borrow.pay_fine == true ? (
-                            <td className="px-6 py-4 text-center">
+                          <td className="px-6 py-4 text-center">
+                            {borrow.pay_fine == true ? (
                               <button type="button" className="focus:outline-none text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300   font-medium rounded-lg text-sm px-5 py-2.5 mb-2" onClick={() => payFine(borrow.id)}>{t('user_book20')}</button>
-                            </td>
-                          ) : (
-                            <td className="px-6 py-4 text-center">
-                              -
-                            </td>
-                          )}
+                            ) : (
+                              <>
+                                -
+                              </>
+                            )}
+                          </td>
+                          <td className="px-6 py-4 text-center">
+                            {borrow.status == 'Done' ? (
+                              <div className="rounded-full bg-yellow-400 w-10 h-10 flex items-center justify-center mx-auto" onClick={() => openRatingModal(borrow)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="w-5" fill="#ffffff"><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" /></svg>
+                              </div>
+                            ) : (
+                              <>
+                                Finish Reading First
+                              </>
+                            )}
+                          </td>
                         </tr>
                       ))
                     )}
---
+++ resources/js/Pages/Users/modals/BookRatingModal.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { Inertia } from '@inertiajs/inertia'
+import { FormEvent } from 'react'
+import { useLaravelReactI18n } from 'laravel-react-i18n'
+import toast, { Toaster } from 'react-hot-toast'
+
+interface Borrow {
+    book: any
+    id?: string
+    user_id: string
+    book_id: string
+    amount: string
+    borrow_in: string
+    borrow_out: string
+    status: string
+    pay_fine: boolean
+  }
+  
+interface modalProps {
+    closeModal: () => void
+    borrow: Borrow
+}
+
+export default function BookRatingModal({ borrow, closeModal }: modalProps) {
+    const [name, setName] = useState('')
+    const { t, tChoice, currentLocale, setLocale, getLocales, isLocale, loading } = useLaravelReactI18n()
+
+    const storeCategory = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        Inertia.post('/admin/categories', {
+            name: name
+        })
+        toast.success('Data Successfully Added!')
+    }
+
+    return (
+        <>
+            <Toaster
+                position="top-right"
+                reverseOrder={false}
+            />
+            <div className='fixed w-[100%] h-[100vh] bg-[#1414147c] z-[99] flex justify-center items-center'>
+                <div className='bg-white w-[50%] rounded-lg z-[999] px-6 flex items-center'>
+                    <div className="w-full py-7">
+                        <h1 className='font-bold text-[20px]'>Rate The Book</h1>
+                        <form className="mx-auto mt-5" onSubmit={storeCategory}>
+                            <div className="mb-5">
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">{t('admin64')}</label>
+                                <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={t('admin65')} required />
+                            </div>
+                            <div className="flex">
+                                <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center" onClick={closeModal}>{t('admin54')}</button>
+                                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ml-3">{t('admin55')}</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}   
