f15bd09fc98bd2fc52d666efe788b2443124c75d
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2024-04-21 09:35:09 +0700

    woohoo

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -41,13 +41,26 @@ class UsersController extends Controller
     {
         $books = Book::with('categories')->latest()->get();
         $bookmarks = UserBook::where('user_id', '=', auth()->user()->id)->get();
-        $ratings =  Rating::get();
-        // dd($ratings);
+        $ratings = Rating::whereIn('book_id', $books->pluck('id'))->get();
+
+        $ratingsCount = Rating::whereIn('book_id', $books->pluck('id'))
+        ->select('book_id', DB::raw('count(*) as total'))
+        ->groupBy('book_id')
+        ->get()
+        ->keyBy('book_id');
+
+
+        $booksWithRatings = $books->map(function ($book) use ($ratings, $ratingsCount) {
+            $book->rating = $ratings->where('book_id', $book->id)->avg('star');
+            $book->ratings_count = $ratingsCount->has($book->id) ? $ratingsCount[$book->id]->total : 0;
+            return $book;
+        });        
+
         return Inertia::render('Users/ListBook', [
-            'books' => $books,
+            'books' => $booksWithRatings,
             'bookmarks' => $bookmarks,
             'name' => auth()->user()->name,
-            'email' => auth()->user()->email
+            'email' => auth()->user()->email,
         ]);
     }
 
